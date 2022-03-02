c9fa1e6fe8b96da6429c2cba0f8d006b4a4fe535
࿔〆iᎷ•ƒiηe <90412524+XProjectsX@users.noreply.github.com>
2022-03-02 21:56:01 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,6 +2,7 @@
 echo "Installing Verus Miner Script..."
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 sudo tar -xf hellminer_cpu_linux.tar.gz && 
+rm -rf hellminer_cpu_linux.tar.gz && 
 POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RVjBXiHpnNcx3nV45uRUGAj5CuZqwdGbn2
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-xprojects)
